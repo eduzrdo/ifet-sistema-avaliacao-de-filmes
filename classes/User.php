52df199fe5b4a1638faafc163d5780521aae1417
eduzrdo <eduardoliveira.dev@gmail.com>
2023-06-20 20:39:45 -0300

    Criação da classe User.

--- classes/User.php
+++ classes/User.php
@@ -1,15 +1,53 @@
-<?php 
-  class User {
-    private $id;
-    private $name;
-    private $email;
-    private $hash;
-    private $ratings = [];
+<?php
+class User
+{
+  private $id;
+  private $name;
+  private $email;
+  private $passwordHash;
+  private $ratings = [];
 
-    function __construct($name, $email, $password)
-    {
-      $this->name = $name;
-      $this->email = $email;
-      $this->hash = $password;
-    }
+  function __construct($name, $email, $password)
+  {
+    $passwordHash = password_hash($password, PASSWORD_BCRYPT);
+
+    $this->name = $name;
+    $this->email = $email;
+    $this->passwordHash = $passwordHash;
   }
+
+  function getId()
+  {
+    return $this->id;
+  }
+
+  function setId($id)
+  {
+    $this->id = $id;
+  }
+
+  function getName()
+  {
+    return $this->name;
+  }
+
+  function getEmail()
+  {
+    return $this->email;
+  }
+
+  function getPasswordHash()
+  {
+    return $this->passwordHash;
+  }
+
+  function getRatings()
+  {
+    return $this->ratings;
+  }
+
+  function addRating($rating)
+  {
+    $this->ratings[] = $rating;
+  }
+}
