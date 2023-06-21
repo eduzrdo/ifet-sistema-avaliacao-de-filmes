373c3ca4811b63fd6dd1685d375a7d73736b9337
EduardoAlphonse <eduardoliveira.dev@gmail.com>
2023-06-21 02:13:35 -0300

    Atualizações. Diag. de classes; Class Movies renomeada para Movie; implementações de métodos na classe System.

--- index.php
+++ index.php
@@ -1,15 +1,42 @@
 <?php
 require_once './classes/System.php';
 require_once './classes/User.php';
+require_once './classes/Movie.php';
 
 $system = new System();
 
-$user = new User("Eduardo", "eduardoliveira.dev@gmail.com", "umasenhamuitofortona");
+$user1 = new User("Eduardo", "eduardoliveira.dev@gmail.com", "umasenhamuitofortona");
 $user2 = new User("Luiz Eduardo", "lefn@hotmail.com.br", "senhafracakkk");
 
-// $system->createUser($user);
+$movie1 = new Movie("Harry Potter e o Prisioneiro de Azkaban", "urlDaImagem", "sjdfkasd");
+$movie2 = new Movie("Harry Potter e a Ordem da Fênix", "urlDaImagem", "sjdfkasd");
+$movie3 = new Movie("Harry Potter e o Enigma do Príncipe", "urlDaImagem", "sjdfkasd");
+
+// $system->createUser($user1);
 // $system->createUser($user2);
 
+// $system->createMovie($movie1);
+// $system->createMovie($movie2);
+// $system->createMovie($movie3);
+
 echo '<pre>';
-var_dump($system);
+
+
+// var_dump($system->getUsers());
+
+// $result = $system->findUser("eduardoliveira.dev@gmail.com");
+
+// var_dump($result);
+
+// $auth = $system->authuser("eduardoliveira.dev@gmail.com", "umasenhamuitofortona");
+
+// var_dump($x[1]['name']);
+
+$movie = $system->findMovie(0);
+
+echo '<pre>';
+var_dump($movie);
+echo '</pre>';
+
+
 echo '</pre>';
