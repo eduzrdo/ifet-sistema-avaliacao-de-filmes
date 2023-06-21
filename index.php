0b301c743d27a29f3505ac626e501f2ba1b7cf22
eduzrdo <eduardoliveira.dev@gmail.com>
2023-06-20 21:04:31 -0300

    Testando a classes User.

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
 require_once './classes/System.php';
+require_once './classes/User.php';
 
 $system = new System();
+
+$user = new User("Eduardo", "eduardoliveira.dev@gmail.com", "umasenhamuitofortona");
+
+echo '<pre>';
+var_dump($user->getId());
+echo '</pre>';
