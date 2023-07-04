055ac036cdd798bade8615fd61b00b954faafb84
eduzrdo <eduardoliveira.dev@gmail.com>
2023-07-03 21:34:31 -0300

    Configuração dos estilos globais e criação dos arquivos da página de login

--- index.php
+++ index.php
@@ -1,51 +1,22 @@
-<?php
-require_once './classes/System.php';
-require_once './classes/User.php';
-require_once './classes/Movie.php';
-require_once './classes/Rating.php';
-$system = new System();
-
-$user1 = new User("Eduardo", "eduardoliveira.dev@gmail.com", "umasenhamuitofortona");
-$movie1 = new Movie("Harry Potter e o Prisioneiro de Azkaban", "urlDaImagem");
-$rating1 = new Rating($user1, $movie1, 3, "O melhor filme da saga!!");
-$rating2 = new Rating($user1, $movie1, 4, "O melhor filme da saga!!");
-
-echo $movie1->getAverageScore();
-
-$system->createUser($user1);
-// $system->createMovie($movie1);
-// $system->createRating($rating1);
-
-$result = $system->findUser("eduardoliveira.dev@gmail.com");
-
-echo '<pre>';
-var_dump($user1->getEmail());
-echo '</pre>';
-
-echo '<pre>';
-var_dump($result[1]->getEmail());
-echo '</pre>';
-
-echo '<pre>';
-var_dump($user1->getEmail() == $result[1]->getEmail());
-echo '</pre>';
-
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link rel="stylesheet" href="./styles/global.css">
+
+    <style>
+        h1 {
+            color: var(--red-text);
+        }
+    </style>
 </head>
 
 <body>
-    <form action="./filmes/procurar.php" method="get">
-        <input type="text" name="search" placeholder="Procurar filme">
-        <button type="submit">Procurar</button>
-    </form>
+    <h1>TEXTO</h1>
 </body>
 
 </html>
---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./styles/global.css">
+  <link rel="stylesheet" href="./styles/login.css">
+
+  <title>Login - </title>
+</head>
+
+<body>
+  <label>
+    <input type="email" name="" id="">
+  </label>
+  <button>ENTRAR</button>
+</body>
+
+</html>
