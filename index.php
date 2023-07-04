c75706a552d150ee6ceaaaeee824b4d2bbcbc48f
MauryanDev <mauryan.dev@gmail.com>
2023-07-04 18:18:06 -0300

    csshome

--- index.php
+++ index.php
@@ -4,19 +4,50 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
+    <title>Home</title>
     <link rel="stylesheet" href="./styles/global.css">
-
-    <style>
-        h1 {
-            color: var(--red-text);
-        }
-    </style>
+    <link rel="stylesheet" href="./styles/home.css">
 </head>
 
 <body>
-    <h1>TEXTO</h1>
+    <section class="backgroundIMG">
+        <div class="gradient"></div>
+        <div class="content-all">
+            <nav class="header">
+                <div class="logo">
+                    <h1>StarFilmes</h1>
+                </div>
+                <div class="navigation">
+                    <p>Principal</p>
+                    <p>Melhores Avaliados</p>
+                    <p>Mais Avalaidos</p>
+                </div>
+                <div>
+                    <input type="text"><img src="" alt=""></input>
+                </div>
+            </nav>
+            <header class="content">
+                <h1>The Witcher</h1>
+                <p>1 Temporada</p>
+                <p>5 estrelas</p>
+                <div class="genres">
+                    <p>Crime</p>
+                    <p>Drama</p>
+                    <p>Misterio</p>
+                </div>
+                <button>Ver Comentarios</button>
+            </header>
+            <footer class="carousel">
+                <img src="https://cdn.ome.lt/NFcSrPoyGkmpMAf20eH0CNaAf6M=/770x0/smart/uploads/conteudo/fotos/3606754-witcher_poster.jpg" alt="">
+                <img src="https://cdn.ome.lt/NFcSrPoyGkmpMAf20eH0CNaAf6M=/770x0/smart/uploads/conteudo/fotos/3606754-witcher_poster.jpg" alt="">
+                <img src="https://cdn.ome.lt/NFcSrPoyGkmpMAf20eH0CNaAf6M=/770x0/smart/uploads/conteudo/fotos/3606754-witcher_poster.jpg" alt="">
+                <img src="https://cdn.ome.lt/NFcSrPoyGkmpMAf20eH0CNaAf6M=/770x0/smart/uploads/conteudo/fotos/3606754-witcher_poster.jpg" alt="">
+                <img src="https://cdn.ome.lt/NFcSrPoyGkmpMAf20eH0CNaAf6M=/770x0/smart/uploads/conteudo/fotos/3606754-witcher_poster.jpg" alt="">
+                <img src="https://cdn.ome.lt/NFcSrPoyGkmpMAf20eH0CNaAf6M=/770x0/smart/uploads/conteudo/fotos/3606754-witcher_poster.jpg" alt="">
+                <img src="https://cdn.ome.lt/NFcSrPoyGkmpMAf20eH0CNaAf6M=/770x0/smart/uploads/conteudo/fotos/3606754-witcher_poster.jpg" alt="">
+            </footer>
+        </div>
+    </section>
 </body>
 
 </html>
