c045fb13ce3219fbb1edbf4fce4c9f26125aa1a5
MauryanDev <mauryan.dev@gmail.com>
2023-07-05 14:57:43 -0300

    home2.0

--- index.php
+++ index.php
@@ -4,7 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Pag Inicial</title>
+    <script src="https://unpkg.com/@phosphor-icons/web"></script>
+    <script src="./styles/jsHome/home.js"></script>
     <link rel="stylesheet" href="./styles/global.css">
     <link rel="stylesheet" href="./styles/home.css">
 </head>
@@ -13,39 +15,44 @@
     <section class="backgroundIMG">
         <div class="gradient"></div>
         <div class="content-all">
-            <nav class="header">
+            <nav class="header page-header">
                 <div class="logo">
-                    <h1>StarFilmes</h1>
+                    <img src="../ifet-sistema-avaliacao-de-filmes/assets/starfilms-logo.svg" alt="foda-se se vc n encherga">
                 </div>
-                <div class="navigation">
+                <div class="navigation nav-text">
                     <p>Principal</p>
                     <p>Melhores Avaliados</p>
                     <p>Mais Avalaidos</p>
                 </div>
-                <div>
-                    <input type="text"><img src="" alt=""></input>
+                <div class="search" onmouseleave="animationClose()">
+                    <input type="text" id="input-search" placeholder="Busca" ><i class="ph ph-magnifying-glass" onclick="animationOpen()" ></i>
                 </div>
             </nav>
             <header class="content">
-                <h1>The Witcher</h1>
+                <h1 class="title">The Witcher</h1>
                 <p>1 Temporada</p>
-                <p>5 estrelas</p>
+                <div class="star">
+                <i class="ph-fill ph-star"></i>
+                <i class="ph-fill ph-star"></i>
+                <i class="ph-fill ph-star"></i>
+                <i class="ph ph-star"></i>
+                <i class="ph ph-star"></i>
+                <span class="score button-text">3.5</span>
+                </div>
                 <div class="genres">
                     <p>Crime</p>
                     <p>Drama</p>
                     <p>Misterio</p>
                 </div>
-                <button>Ver Comentarios</button>
+                <button  class="button-text">Ver Comentarios</button>
             </header>
             <footer class="carousel">
-                <img src="https://cdn.ome.lt/NFcSrPoyGkmpMAf20eH0CNaAf6M=/770x0/smart/uploads/conteudo/fotos/3606754-witcher_poster.jpg" alt="">
-                <img src="https://cdn.ome.lt/NFcSrPoyGkmpMAf20eH0CNaAf6M=/770x0/smart/uploads/conteudo/fotos/3606754-witcher_poster.jpg" alt="">
-                <img src="https://cdn.ome.lt/NFcSrPoyGkmpMAf20eH0CNaAf6M=/770x0/smart/uploads/conteudo/fotos/3606754-witcher_poster.jpg" alt="">
-                <img src="https://cdn.ome.lt/NFcSrPoyGkmpMAf20eH0CNaAf6M=/770x0/smart/uploads/conteudo/fotos/3606754-witcher_poster.jpg" alt="">
-                <img src="https://cdn.ome.lt/NFcSrPoyGkmpMAf20eH0CNaAf6M=/770x0/smart/uploads/conteudo/fotos/3606754-witcher_poster.jpg" alt="">
-                <img src="https://cdn.ome.lt/NFcSrPoyGkmpMAf20eH0CNaAf6M=/770x0/smart/uploads/conteudo/fotos/3606754-witcher_poster.jpg" alt="">
-                <img src="https://cdn.ome.lt/NFcSrPoyGkmpMAf20eH0CNaAf6M=/770x0/smart/uploads/conteudo/fotos/3606754-witcher_poster.jpg" alt="">
-            </footer>
+                <p class="movie-emphasis"><img src="https://cdn.ome.lt/NFcSrPoyGkmpMAf20eH0CNaAf6M=/770x0/smart/uploads/conteudo/fotos/3606754-witcher_poster.jpg" alt="">The Witcher</p> 
+                <p class="movie-emphasis"><img src="https://cdn.ome.lt/NFcSrPoyGkmpMAf20eH0CNaAf6M=/770x0/smart/uploads/conteudo/fotos/3606754-witcher_poster.jpg" alt="">The Witcher</p> 
+                <p class="movie-emphasis"><img src="https://i0.wp.com/almanaque21.com.br/wp-content/uploads/2019/12/CAPA-1.png?fit=2490%2C3201&ssl=1" alt="">Star Wars</p> 
+                <p class="movie-emphasis"><img src="https://cdn.ome.lt/NFcSrPoyGkmpMAf20eH0CNaAf6M=/770x0/smart/uploads/conteudo/fotos/3606754-witcher_poster.jpg" alt="">The Witcher</p> 
+                <p class="movie-emphasis"><img src="https://cdn.ome.lt/NFcSrPoyGkmpMAf20eH0CNaAf6M=/770x0/smart/uploads/conteudo/fotos/3606754-witcher_poster.jpg" alt="">The Witcher</p> 
+                </footer>
         </div>
     </section>
 </body>
