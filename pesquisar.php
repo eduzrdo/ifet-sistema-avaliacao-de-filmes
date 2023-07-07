7b6ebb708ae157339266c7f78c605f934b9eda30
luiz <lefn@homail.com.br>
2023-07-06 22:17:40 -0300

    paginas de resultados

--- pesquisar.php
+++ pesquisar.php
@@ -6,14 +6,58 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
   <link rel="stylesheet" href="styles/global.css">
+  <link rel="stylesheet" href="styles/pesquisar.css">
 
   <title>Resultados - </title>
 </head>
 
 <body>
+
+  <div class="background-plane">
+
+    <img class="img" src="https://image.tmdb.org/t/p/w300/9Xcg7Ar4ketv4rl8yeK32yp9zQA.jpg">
+    <div></div>
+  </div>
+
   <?php require_once 'components/Header.php' ?>
 
-  <h1>asdfalsdj</h1>
-</body>
+  <div class="movie-grid">
+    
+      <a href="" class="movie-card">
+        <img src="https://image.tmdb.org/t/p/w300/9Xcg7Ar4ketv4rl8yeK32yp9zQA.jpg">
+        <span class="body-text">oi</span>
+      </a>
+    
+      <a href="" class="movie-card">
+        <img src="https://image.tmdb.org/t/p/w300/9Xcg7Ar4ketv4rl8yeK32yp9zQA.jpg">
+        <span class="body-text">oi</span>
+      </a>
+    
+      <a href="" class="movie-card">
+        <img src="https://image.tmdb.org/t/p/w300/9Xcg7Ar4ketv4rl8yeK32yp9zQA.jpg">
+        <span class="body-text">oi</span>
+      </a>
+    
+      <a href="" class="movie-card">
+        <img src="https://image.tmdb.org/t/p/w300/9Xcg7Ar4ketv4rl8yeK32yp9zQA.jpg">
+        <span class="body-text">oi</span>
+      </a>
+    
+      <a href="" class="movie-card">
+        <img src="https://image.tmdb.org/t/p/w300/9Xcg7Ar4ketv4rl8yeK32yp9zQA.jpg">
+        <span class="body-text">oi</span>
+      </a>
+    
+      <a href="" class="movie-card">
+        <img src="https://image.tmdb.org/t/p/w300/9Xcg7Ar4ketv4rl8yeK32yp9zQA.jpg">
+        <span class="body-text">oi</span>
+      </a>
+    
+      <a href="" class="movie-card">
+        <img src="https://image.tmdb.org/t/p/w300/9Xcg7Ar4ketv4rl8yeK32yp9zQA.jpg">
+        <span class="body-text">oi</span>
+      </a>
+  </div>
+</body >
 
 </html>
