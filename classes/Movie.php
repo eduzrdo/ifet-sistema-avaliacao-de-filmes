0ba87970aacd79fed580f2973061aa6d5bec19a9
eduzrdo <eduardoliveira.dev@gmail.com>
2023-07-25 22:24:01 -0300

    Funcionalidade de selecionar filmes na página inicial pronto.

--- classes/Movie.php
+++ classes/Movie.php
@@ -8,6 +8,7 @@ class Movie
     private $averageScore = null;
     private $overview;
     private $ratings = [];
+    private
 
     function __construct($id, $title, $posterPath, $backdropPath, $overview)
     {
--- index.php
+++ index.php
@@ -4,15 +4,18 @@ require_once 'utils/movie.php';
 
 $system = new System();
 
-if (isset($_GET['id'])) {
-    if ($_GET['id'] >= 0) {
-        $movie = $system->getMostRatedMovies()[$_GET['id']];
-    }
+function mapper($movie)
+{
+    return [
+        "id" => $movie->getId(),
+        "title" => $movie->getTitle(),
+        "averageScore" => $movie->getAverageScore(),
+        "posterPath" => $movie->getPosterPath(),
+        "backdropPath" => $movie->getBackdropPath(),
+    ];
 }
 
-if (!isset($_GET['id'])) {
-    $movie = $system->getMostRatedMovies()[0];
-}
+$mostRatedMoviesArray = array_map('mapper', $system->getMostRatedMovies());
 ?>
 
 <!DOCTYPE html>
@@ -33,7 +36,7 @@ if (!isset($_GET['id'])) {
 
 <body>
     <div class="background-plane">
-        <img class="" src="<?php echo makeMovieBackdropPath($movie->getBackdropPath()); ?>" alt="Plano de fundo de <?php echo $movie->getTitle(); ?>">
+        <img class="background-image" src="<?php echo makeMovieBackdropPath($mostRatedMoviesArray[0]["backdropPath"]); ?>" alt="Plano de fundo de <?php echo $mostRatedMoviesArray[0]["title"]; ?>">
         <div></div>
     </div>
 
@@ -41,19 +44,21 @@ if (!isset($_GET['id'])) {
 
     <main>
         <div class="movie-data">
-            <h1 class="title"><?php echo $movie->getTitle(); ?></h1>
+            <h1 class="title"><?php echo $mostRatedMoviesArray[0]["title"]; ?></h1>
 
             <div class="score">
-                <?php
-                $filledStars = floor($movie->getAverageScore());
+                <div class="score-stars">
+                    <?php
+                    $filledStars = floor($mostRatedMoviesArray[0]["averageScore"]);
 
-                for ($i = 0; $i < $filledStars; $i++) {
-                    echo "<i class='ph-fill ph-star'></i>";
-                }
-                for ($i = 0; $i < 5 - $filledStars; $i++) {
-                    echo "<i class='ph ph-star'></i>";
-                }  ?>
-                <span class="scorest button-text"><?php echo number_format($movie->getAverageScore(), 1, ".", "") ?></span>
+                    for ($i = 0; $i < $filledStars; $i++) {
+                        echo "<i class='ph-fill ph-star'></i>";
+                    }
+                    for ($i = 0; $i < 5 - $filledStars; $i++) {
+                        echo "<i class='ph ph-star'></i>";
+                    }  ?>
+                </div>
+                <span class="scorest button-text"><?php echo number_format($mostRatedMoviesArray[0]["averageScore"], 1, ".", "") ?></span>
             </div>
 
             <div class="genres">
@@ -62,23 +67,73 @@ if (!isset($_GET['id'])) {
                 <span class="body-text-small">Família</span>
             </div>
 
-            <a class="button button-secondary" href="filme.php?movieId=<?php echo $movie->getId(); ?>">Ver avaliações</a>
+            <a class="button button-secondary movie-link" href="filme.php?movieId=<?php echo $mostRatedMoviesArray[0]["id"]; ?>">Ver avaliações</a>
         </div>
 
         <div class="best-movies">
             <?php
-            foreach ($system->getMostRatedMovies() as $movieId => $movie) {
+            foreach ($mostRatedMoviesArray as $movie) {
                 echo "
-                <a href=index.php?id=$movieId class='movie-card'>
-                    <i class='ph-fill ph-star'> Top " . $movieId + 1 . "</i>
-                    <img src='" . makeMoviePoster($movie->getPosterPath()) . "' alt='" . $movie->getTitle() . "'>
-                    <span class='body-text'>" . $movie->getTitle() . "</span>
-                </a>
+                <button href=index.php?id=" . $movie['id'] . " class='movie-card' data-backdroppath='" . makeMovieBackdropPath($movie['backdropPath']) . "' data-title='" . $movie['title'] . "' data-id='" . $movie['id'] . "' data-id='" . $movie['id'] . "' data-averageScore='" . $movie['averageScore'] . "''>
+                    <img src='" . makeMoviePoster($movie['posterPath']) . "' alt='" . $movie['title'] . "'>
+                    <span class='body-text'>" . $movie['title'] . "</span>
+                </button>
                 ";
             }
             ?>
         </div>
     </main>
+
+    <script>
+        const movieLinkElement = document.querySelector('.movie-link');
+        const titleElement = document.querySelector('.title');
+        const scoreElement = document.querySelector('.scorest');
+        const backgroundElement = document.querySelector('.background-image');
+        const scoreStarsContainer = document.querySelector('.score-stars');
+
+        const filledStar = document.createElement("i");
+        filledStar.className = 'ph-fill ph-star';
+        const emptyStar = document.createElement("i");
+        emptyStar.className = 'ph ph-star';
+
+        function selectMovie(event) {
+            const movieId = event.currentTarget.getAttribute('data-id');
+            const movieTitle = event.currentTarget.getAttribute('data-title');
+            const movieAverageScore = event.currentTarget.getAttribute('data-averageScore');
+            const movieBackdropPath = event.currentTarget.getAttribute('data-backdroppath');
+
+            titleElement.innerText = movieTitle;
+            backgroundElement.src = movieBackdropPath;
+            movieLinkElement.href = 'filme.php?movieId=' + movieId;
+            scoreElement.innerText = Number(movieAverageScore).toFixed(1).replace(',', '.');
+
+            const scoreAbsolute = Math.floor(movieAverageScore);
+
+            let firstStar = scoreStarsContainer.firstElementChild;
+
+            while (firstStar) {
+                firstStar.remove();
+                firstStar = scoreStarsContainer.firstElementChild;
+            }
+
+            for (let i = 0; i < scoreAbsolute; i++) {
+                const filledStar = document.createElement("i");
+                filledStar.className = 'ph-fill ph-star';
+                scoreStarsContainer.appendChild(filledStar);
+            }
+
+            for (let i = 0; i < 5 - scoreAbsolute; i++) {
+                const emptyStar = document.createElement("i");
+                emptyStar.className = 'ph ph-star';
+                scoreStarsContainer.appendChild(emptyStar);
+            }
+        }
+
+        const buttons = Array.from(document.querySelectorAll('.movie-card'));
+        buttons.forEach(button => {
+            button.onclick = selectMovie;
+        });
+    </script>
 </body>
 
 </html>
