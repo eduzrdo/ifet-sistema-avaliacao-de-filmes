fbfa2aae90ba8fc20c38ac15eb097ba6eba22128
EduardoAlphonse <eduardoliveira.dev@gmail.com>
2023-07-27 18:18:13 -0300

    Remover arquivos desnecessários

--- styles/jsHome/home.js
+++
@@ -1,9 +0,0 @@
-function animationOpen() {
-    const input = document.getElementById("input-search")
-    input.style.width = '200px';
-    input.focus()
-}
-function animationClose() {
-    document.getElementById("input-search").style.width = '0px';
-
-}
